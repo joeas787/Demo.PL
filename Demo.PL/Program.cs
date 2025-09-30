1f2793aa438d60384b42baf97e912547f4ee457b
joeas787 <yousefashraf43434@gmail.com>
2025-09-30 15:28:32 +0200

    mvc7

--- Demo.PL/Program.cs
+++ Demo.PL/Program.cs
@@ -11,6 +11,7 @@ namespace Demo.PL
     {
         public static void Main(string[] args)
         {
+            //
             var builder = WebApplication.CreateBuilder(args);
             //
             // Add services to the container.
